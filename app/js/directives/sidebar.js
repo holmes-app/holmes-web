c76eba64792cef04c561a44511f7068135db2d5b
Evandro Flores <eof@eof.com.br>
2013-10-29 01:32:55 -0200

    Show current worker.review.page.url

--- app/js/directives/sidebar.js
+++ app/js/directives/sidebar.js
@@ -12,7 +12,7 @@
             return $scope.workers = activeWorkers;
           });
         };
-        $timeout(getWorkers, 1000);
+        $timeout(getWorkers, 3000);
         $scope.mostCommonViolations = [
           {
             name: "Javascript total size is too big",
