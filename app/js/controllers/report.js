0c62189d3d794e01adaf0e5e573b47dca526ecbc
Marcelo Jorge Vieira <metal@alucinados.com>
2013-12-18 17:11:40 -0200

    Removing charts

--- app/js/controllers/report.js
+++ app/js/controllers/report.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
   angular.module('holmesApp').controller('ReportCtrl', function($scope, $routeParams, Restangular, $sce) {
-    var buildCharts, isValidDate, updateChartData, updateDetails, updateReviews;
+    var isValidDate, updateDetails, updateReviews;
     $scope.asHtml = function(text) {
       return $sce.trustAsHtml(text);
     };
@@ -26,76 +26,8 @@
         return $scope.model.reviews = reviews;
       });
     };
-    updateChartData = function() {
-      return Restangular.one('page', $routeParams.pageId).getList('violations-per-day').then(function(violations) {
-        violations = violations.violations;
-        return buildCharts(violations);
-      });
-    };
-    buildCharts = function(violations) {
-      var dt, obj, violationCount, violationCountData, violationPoints, violationPointsData, _i, _len;
-      violationPoints = [];
-      violationCount = [];
-      for (_i = 0, _len = violations.length; _i < _len; _i++) {
-        obj = violations[_i];
-        dt = new Date(obj.completedAt);
-        if (!isValidDate(dt)) {
-          continue;
-        }
-        violationPoints.push({
-          x: new Date(obj.completedAt),
-          y: obj.violation_points
-        });
-        violationCount.push({
-          x: new Date(obj.completedAt),
-          y: obj.violation_count
-        });
-      }
-      violationPointsData = [
-        {
-          values: violationPoints,
-          key: 'Violation Points',
-          color: '#ff7f0e'
-        }
-      ];
-      violationCountData = [
-        {
-          values: violationCount,
-          key: 'Violation Count',
-          color: '#2ca02c'
-        }
-      ];
-      nv.addGraph(function() {
-        var chart;
-        chart = nv.models.lineChart();
-        chart.yAxis.tickFormat(d3.format('.02f'));
-        chart.xAxis.tickFormat(function(d) {
-          return (d3.time.format("%d-%b"))(new Date(d));
-        });
-        d3.select('#report-violation-count-chart svg').datum(violationCountData).transition().duration(500).call(chart);
-        nv.utils.windowResize(function() {
-          return d3.select('#report-violation-count-chart svg').call(chart);
-        });
-        return chart;
-      });
-      return nv.addGraph(function() {
-        var chart;
-        chart = nv.models.lineChart();
-        chart.yAxis.tickFormat(d3.format('.02f'));
-        chart.xAxis.tickFormat(function(d) {
-          return (d3.time.format("%d-%b"))(new Date(d));
-        });
-        d3.select('#report-violation-points-chart svg').datum(violationPointsData).transition().duration(500).call(chart);
-        nv.utils.windowResize(function() {
-          return d3.select('#report-violation-points-chart svg').call(chart);
-        });
-        return chart;
-      });
-    };
-    buildCharts([]);
     updateDetails();
-    updateReviews();
-    return updateChartData();
+    return updateReviews();
   });
 
 }).call(this);
