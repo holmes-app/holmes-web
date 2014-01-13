0d9e2890d1991c62bf5c3e332a30aef33e2eddb9
Marcelo Jorge Vieira <metal@alucinados.com>
2014-01-13 18:12:41 -0200

    Violations: Removed pagination

--- app/js/controllers/violation.js
+++ app/js/controllers/violation.js
@@ -1,50 +1,12 @@
 (function() {
   'use strict';
   angular.module('holmesApp').controller('ViolationCtrl', function($scope, $routeParams, Restangular, WebSocket) {
-    var updatePager, updateViolation;
-    $scope.model = {
-      currentPage: 1,
-      numberOfPages: 0
-    };
-    $scope.getClass = function(pageIndex) {
-      if (pageIndex === $scope.model.currentPage) {
-        return 'active';
-      }
-      if (pageIndex !== $scope.model.currentPage) {
-        return '';
-      }
-    };
+    var updateViolation;
+    $scope.model = {};
     updateViolation = function() {
-      return Restangular.one('violation', $routeParams.keyName).get({
-        current_page: $scope.model.currentPage
-      }).then(function(items) {
-        $scope.model.violation = items;
-        return updatePager(items);
+      return Restangular.one('violation', $routeParams.keyName).get().then(function(items) {
+        return $scope.model.violation = items;
       });
-    };
-    updatePager = function(domainData) {
-      var i, _i, _ref, _ref1, _results;
-      if (domainData != null) {
-        $scope.model.numberOfPages = Math.ceil(domainData.reviewCount / 10);
-      }
-      if ($scope.model.currentPage < 6) {
-        $scope.model.nextPages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-      }
-      $scope.model.prevPage = Math.max(1, $scope.model.currentPage - 5);
-      $scope.model.nextPage = Math.max(6, Math.min($scope.model.numberOfPages, $scope.model.currentPage + 5));
-      if ($scope.model.currentPage >= 6) {
-        $scope.model.nextPages = [];
-        _results = [];
-        for (i = _i = _ref = $scope.model.currentPage - 4, _ref1 = $scope.model.currentPage + 4; _i <= _ref1; i = _i += 1) {
-          _results.push($scope.model.nextPages.push(i));
-        }
-        return _results;
-      }
-    };
-    $scope.goToReviewPage = function(pageIndex) {
-      $scope.model.currentPage = pageIndex;
-      updatePager();
-      return updateViolation();
     };
     return updateViolation();
   });
