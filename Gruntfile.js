e26a7652d06df7e0c036218bb93ab6b8fe4e1fc5
Marcelo Jorge Vieira <metal@alucinados.com>
2014-07-17 17:47:33 -0300

    Fixed css3-social-signin-buttons images path

--- Gruntfile.js
+++ Gruntfile.js
@@ -370,6 +370,12 @@ module.exports = function (grunt) {
         pattern: '../flags/',
         replacement: '../bower_components/flag-icon-css/flags/',
         recursive: false
+      },
+      replace_social_signin_buttons_image_path: {
+        path: 'dist/styles/vendor.css',
+        pattern: 'auth-icons.png',
+        replacement: '../bower_components/css3-social-signin-buttons/auth-icons.png',
+        recursive: false
       }
     },
 
